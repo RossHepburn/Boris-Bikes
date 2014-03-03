399023eb32490b5e9353c869cd9e3165c784f9f5
Ross Hepburn <hepburnjrl@gmail.com>
2014-03-03 19:40:09 +0000

    Capacity Added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,7 @@ describe DockingStation do
 
 	let(:bike) {Bike.new}
 	let(:station) {DockingStation.new}
+	let(:station) { DockingStation.new(:capacity => 20) }
 
 	it "should accept a bike" do
 		bike = Bike.new
@@ -20,4 +21,9 @@ describe DockingStation do
 		expect(station.bike_count).to eq(0)
 	end
 
+	it "should know when it is full" do
+		expect(station).not_to be_full
+		20.times { station.dock(Bike.new) }
+		expect(station).to be_full
+	end
 end
