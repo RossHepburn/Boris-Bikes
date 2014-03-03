8bfbd793eaabaadde59df797c936b0a02b1f3a40
Ross Hepburn <hepburnjrl@gmail.com>
2014-03-03 18:08:42 +0000

    Refactoring

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,7 +1,7 @@
 class Bike
 	
 	def inititialize
-		@broken = false
+		fix
 	end
 	
 	def broken?
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,20 +1,19 @@
 require_relative "../lib/bike"
 
 describe Bike do
+
+	let(:bike) {Bike.new}
 	
 	it "should not be broken after we create it" do
-		bike = Bike.new 
 		expect(bike).not_to be_broken
 	end
 
 	it "should be able to break" do 
-		bike = Bike.new
 		bike.break
 		expect(bike).to be_broken
 	end
-
+	
 	it "should be able to be fixed" do
-		bike = Bike.new
 		bike.break
 		bike.fix
 		expect(bike).not_to be_broken
