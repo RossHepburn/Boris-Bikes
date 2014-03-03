5df7c621c04649ecf15565b8d70eae6234b193e8
Ross Hepburn <hepburnjrl@gmail.com>
2014-03-03 19:58:38 +0000

    Error if docking full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -23,7 +23,16 @@ describe DockingStation do
 
 	it "should know when it is full" do
 		expect(station).not_to be_full
-		20.times { station.dock(Bike.new) }
+		fill_station station		
 		expect(station).to be_full
+	end
+
+	it "should not accept a bike when it is full" do
+		fill_station station
+		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
+	end
+
+	def fill_station(station)
+		20.times { station.dock(Bike.new) }
 	end
 end
